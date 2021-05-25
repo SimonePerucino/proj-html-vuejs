62d39c25c2d3ffabab5a0dabd26198acfebb39b2
SimonePerucino <simone.perucino@gmail.com>
2021-05-25 12:37:34 +0200

    Inizializzazione

---
+++ appVue.js
@@ -0,0 +1,28 @@
+new Vue({
+    el:"#app",
+    data: {
+        navbarLinks:[{
+            text:"Home",
+            link: "",
+        },
+        {
+            text:"Pages",
+            link: "#",
+        },
+        {
+            text:"Blog",
+            link: "#",
+        },
+        {
+            text:"Shop",
+            link: "#",
+        }],
+        footeLinks:[
+            {
+                
+            }
+        ]
+    },
+    methods:{},
+    mounted(){},
+})
